96b6bcc03cf16a71d45e81590037f0ce74ccd84d
OEfren <efren.ordaz.mendoza@gmail.com>
2021-09-13 11:58:28 -0500

    Update Encrypt.cs

--- WSVenta/Tools/Encrypt.cs
+++ WSVenta/Tools/Encrypt.cs
@@ -21,5 +21,7 @@ namespace WSVenta.Tools
             for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
             return sb.ToString();
         }
+        
+        // TODO: Falta desencriptar.
     }
 }
